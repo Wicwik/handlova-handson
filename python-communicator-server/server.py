0703db609be95dba7a7e64cdff8755c1129f7629
Róbert Belanec <belanecrobert22@gmail.com>
2020-02-06 11:13:50 +0100

    basic server created

---
+++ python-communicator-server/server.py
@@ -0,0 +1,12 @@
+import socket
+
+SERVER_IP = '127.0.0.1'
+SERVER_PORT = 12345
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+sock.bind((SERVER_IP, SERVER_PORT))
+
+while True:
+    data, addr = sock.recvfrom(1024)
+    print('recieved message: ' + data)
