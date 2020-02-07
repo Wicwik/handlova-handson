6378484a77c676b1dc148cd13dabfd1fbdaa8b6d
Róbert Belanec <belanecrobert22@gmail.com>
2020-02-07 15:07:36 +0100

    message types(ACK, MSG, FILE) added

--- python-communicator-client/client.py
+++ python-communicator-client/client.py
@@ -1,9 +1,8 @@
 import socket
 
-CLIENT_IP = '127.0.0.1'
-CLIENT_PORT = 12345
-MESSAGE = 'Hello my friend'
+addr = ('127.0.0.1', 12345) # toto je tuple
+message = 'Hello my friend'
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-sock.sendto(MESSAGE.encode('utf-8'), (CLIENT_IP, CLIENT_PORT))
+sock.sendto(message.encode('utf-8'), addr)
--- python-communicator-server/server.py
+++ python-communicator-server/server.py
@@ -1,13 +1,19 @@
 import socket
 
-SERVER_IP = '127.0.0.1'
-SERVER_PORT = 12345
+ACK = 1
+MSG = 1 << 1
+FILE = 1 << 2
+
+addr = ('127.0.0.1', 12345)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-sock.bind((SERVER_IP, SERVER_PORT))
+sock.bind(addr)
 
-while True:
-    data, addr = sock.recvfrom(1024)
-    print('Recieved message: ' + data.decode('utf8'))
-    print('From: ' + addr[0] + ':' + str(addr[1]))
+# while True:
+#     data, addr = sock.recvfrom(1024) # velkost buffera
+#     print('Recieved message: ' + data.decode('utf8'))
+#     print('From: ' + addr[0] + ':' + str(addr[1]))
+
+
+sock.close()
