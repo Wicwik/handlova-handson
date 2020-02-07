640af564ec3a4c70ef0d2a783593d924d688bbd8
Róbert Belanec <belanecrobert22@gmail.com>
2020-02-07 15:25:40 +0100

    ulozit si tuto verziu, dobre sa na nej ukaze ako to funguje

--- python-communicator-server/server.py
+++ python-communicator-server/server.py
@@ -4,16 +4,20 @@ ACK = 1
 MSG = 1 << 1
 FILE = 1 << 2
 
+def accept_packets():
+    packets_accepted = 0;
+    while True:
+        data, addr = sock.recvfrom(1024) # velkost buffera
+        packets_accepted = packets_accepted + 1
+        print('Recieved ' + str(packets_accepted) + '. packet:', end='')
+        print(' - From: ' + addr[0] + ':' + str(addr[1]))
+
 addr = ('127.0.0.1', 12345)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 sock.bind(addr)
 
-# while True:
-#     data, addr = sock.recvfrom(1024) # velkost buffera
-#     print('Recieved message: ' + data.decode('utf8'))
-#     print('From: ' + addr[0] + ':' + str(addr[1]))
-
+accept_packets()
 
 sock.close()
