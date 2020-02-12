68dce74ac19ae3de636d5f81d5f1902a8f5c2bb3
Róbert Belanec <belanecrobert22@gmail.com>
2020-02-12 11:25:55 +0100

    client part done

--- python-communicator-client/client.py
+++ python-communicator-client/client.py
@@ -1,8 +1,41 @@
 import socket
+from io import StringIO
 
-addr = ('127.0.0.1', 12345) # toto je tuple
-message = 'Hello my friend'
+ACK = 1
+MSG = 1 << 1
+FILE = 1 << 2
+HEADER_SIZE = 2
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-sock.sendto(message.encode('utf-8'), addr)
+def prepare_data(data,  type, size):
+    type_byte = type.to_bytes(2, byteorder='big')
+    size_byte = size.to_bytes(2, byteorder='big')
+    print(size_byte)
+    data_byte = data.encode('utf-8')
+    packet = type_byte + size_byte + data_byte
+
+    return packet
+
+
+def send_packet(data, size, dest, port):
+    addr = (dest, port) # toto je tuple
+    message = prepare_data(data, MSG, size)
+
+    sock.sendto(message, addr)
+
+input_ip = input('Zadaj cielovu IP: ')
+input_port = int(input('Zadaj port: '), 10)
+input_message = input('Zadaj spravu: ')
+input_size = int(input('Zadaj max velkost dat v PDU: '), 10)
+
+if len(input_message) > input_size:
+    string_file = StringIO(input_message)
+    while True:
+        chunk = string_file.read(input_size)
+        if len(chunk) > 0:
+            send_packet(chunk, input_size, input_ip, input_port)
+        if len(chunk) < input_size:
+            break;
+else:
+    send_packet(input_message, input_size, input_ip, input_port)
--- python-communicator-server/server.py
+++ python-communicator-server/server.py
@@ -2,15 +2,15 @@ import socket
 
 ACK = 1
 MSG = 1 << 1
-FILE = 1 << 2
 
 def accept_packets():
     packets_accepted = 0;
     while True:
-        data, addr = sock.recvfrom(1024) # velkost buffera
+        data, addr = sock.recvfrom(1518) # velkost buffera
         packets_accepted = packets_accepted + 1
         print('Recieved ' + str(packets_accepted) + '. packet:', end='')
         print(' - From: ' + addr[0] + ':' + str(addr[1]))
+        print(data[0:2])
 
 addr = ('127.0.0.1', 12345)
 
