2c2cbfeb545e9a4a8b1a8b92aead1b850aa6346a
Róbert Belanec <belanecrobert22@gmail.com>
2020-02-12 12:09:28 +0100

    done

--- python-communicator-client/client.py
+++ python-communicator-client/client.py
@@ -3,20 +3,17 @@ from io import StringIO
 
 ACK = 1
 MSG = 1 << 1
-FILE = 1 << 2
 HEADER_SIZE = 2
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-def prepare_data(data,  type, size):
+def prepare_data(data, type, size):
     type_byte = type.to_bytes(2, byteorder='big')
     size_byte = size.to_bytes(2, byteorder='big')
-    print(size_byte)
     data_byte = data.encode('utf-8')
     packet = type_byte + size_byte + data_byte
 
     return packet
-
 
 def send_packet(data, size, dest, port):
     addr = (dest, port) # toto je tuple
@@ -24,10 +21,16 @@ def send_packet(data, size, dest, port):
 
     sock.sendto(message, addr)
 
-input_ip = input('Zadaj cielovu IP: ')
-input_port = int(input('Zadaj port: '), 10)
-input_message = input('Zadaj spravu: ')
-input_size = int(input('Zadaj max velkost dat v PDU: '), 10)
+def send_ack(data, size, dest, port):
+    addr = (dest, port)
+    message = prepare_data(data, ACK, size)
+
+    sock.sendto(message, addr)
+
+input_ip = input('Destination IP address: ')
+input_port = int(input('Destination port: '), 10)
+input_message = input('Message: ')
+input_size = int(input('Number of maximum data bytes in PDU: '), 10)
 
 if len(input_message) > input_size:
     string_file = StringIO(input_message)
@@ -37,5 +40,12 @@ if len(input_message) > input_size:
             send_packet(chunk, input_size, input_ip, input_port)
         if len(chunk) < input_size:
             break;
+
+    send_ack('', 0, input_ip, input_port)
 else:
     send_packet(input_message, input_size, input_ip, input_port)
+
+data, addr = sock.recvfrom(1518)
+server_recieved_packets = int.from_bytes(data[4:], byteorder='big')
+
+print('Succesfully sent ' + str(server_recieved_packets) + ' packets to server')
--- python-communicator-server/server.py
+++ python-communicator-server/server.py
@@ -5,12 +5,37 @@ MSG = 1 << 1
 
 def accept_packets():
     packets_accepted = 0;
+    full_message = ''
+
     while True:
         data, addr = sock.recvfrom(1518) # velkost buffera
         packets_accepted = packets_accepted + 1
         print('Recieved ' + str(packets_accepted) + '. packet:', end='')
         print(' - From: ' + addr[0] + ':' + str(addr[1]))
-        print(data[0:2])
+
+        type = data[0:2]
+        data_size = data[2:4]
+        message = data[4:]
+
+        if int.from_bytes(type, byteorder='big') == ACK:
+            break;
+
+        full_message += message.decode('utf-8')
+
+    print('The message is: ' + full_message)
+
+    info = (addr, packets_accepted)
+    return info
+
+def send_ack_to_client(packets_accepted, client_addr):
+    type_byte = ACK.to_bytes(2, byteorder='big')
+    size_byte = (2).to_bytes(2, byteorder='big')
+    data_byte = packets_accepted.to_bytes(2, byteorder='big')
+
+    packet = type_byte + size_byte + data_byte
+
+    sock.sendto(packet, client_addr)
+
 
 addr = ('127.0.0.1', 12345)
 
@@ -18,6 +43,10 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 sock.bind(addr)
 
-accept_packets()
+info = accept_packets()
+client_addr = info[0]
+packets_accepted = info[1]
+
+send_ack_to_client(packets_accepted, client_addr)
 
 sock.close()
